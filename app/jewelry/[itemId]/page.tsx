4534e7c6428f929c3b6555626bb4f112cad64ea6
ZafCode14 <zafarwp14@gmail.com>
2024-08-07 15:55:42 +0300

    item page added images

--- app/jewelry/[itemId]/page.tsx
+++ app/jewelry/[itemId]/page.tsx
@@ -11,6 +11,7 @@ interface Props {
 }
 
 const ItemPage = ({ params }: Props) => {
+  const [activePhoto, setActivePhoto] = useState(1);
   const [width, setWidth] = useState(0);
   const { itemId } = params;
 
@@ -85,7 +86,14 @@ const ItemPage = ({ params }: Props) => {
       <div className='w-[800px] max-w-full flex flex-col'>
         <p className="mt-20 self-start text-color-balck">Украшения &gt; {turnCollectionToName(data[itemId].collection)} &gt; {data[itemId].title}</p>
         <div className='w-[400px] h-[400px] max-w-full'>
-          <Image src={`/images/items/${itemId}/photo1.png`} width={"2000"} height={"2000"} alt={`photo${itemId}`} className="h-full w-full cursor-pointer" priority/>
+          <Image src={`/images/items/${itemId}/photo${activePhoto}.png`} width={"1929"} height={"1080"} alt={`photo${itemId}`} className="h-full w-full cursor-pointer object-cover" priority/>
+        </div>
+        <div className='flex overflow-hidden mt-2'>
+          {data[itemId].photo.map((photo, index) => (
+            <Image key={photo} src={`/images/items/${itemId}/${photo}.png`} width={"400"} height={"100"} alt={`photo${itemId}`} className="h-[50px] w-[50px] cursor-pointer object-cover mr-2" priority onClick={() => setActivePhoto(index + 1)} style={{
+              border: index + 1 === activePhoto ? "2px solid black" : "1px solid gray"
+            }}/>
+          ))}
         </div>
         <p>{data[itemId].title}</p>
         <p>{formatPrice(data[itemId].price)} руб</p>
