d9328768ea6ccc0db9ab03fd6c3d51e00ddc27fe
ZafCode14 <zafarwp14@gmail.com>
2024-08-26 23:38:39 +0300

    out

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true, // Ensures that every page is served with a trailing slash (e.g., /about/ instead of /about)
   images: {
     unoptimized: true,
   },
